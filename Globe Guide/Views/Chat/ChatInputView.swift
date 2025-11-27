facb3f75f808b0de7919b0c9310b6fd52c2ad9e8
Necabet Sofiyeva <sofiyevanecabat@gamil.com>
2025-11-27 19:43:31 +0400

    gpt ile kodu deyisdin. sonra duzeldilecek

--- Globe Guide/Views/Chat/ChatInputView.swift
+++ Globe Guide/Views/Chat/ChatInputView.swift
@@ -54,3 +54,18 @@ class ChatInputView: UIView {
         textField.text = ""
     }
 }
+    // chat gpt terefinden verilmis hex rengler. rgb ye sonra kecid edersen
+extension UIColor {
+    convenience init(hex: String) {
+        var hexFormatted: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if hexFormatted.hasPrefix("#") { hexFormatted.remove(at: hexFormatted.startIndex) }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        let r = CGFloat((rgbValue & 0xFF0000) >> 16)/255.0
+        let g = CGFloat((rgbValue & 0x00FF00) >> 8)/255.0
+        let b = CGFloat(rgbValue & 0x0000FF)/255.0
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
+}
