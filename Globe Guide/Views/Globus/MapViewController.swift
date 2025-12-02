7f7418bf30a4d9daca964493d934c3a1878e53c9
Necabet Sofiyeva <sofiyevanecabat@gamil.com>
2025-12-02 20:08:27 +0400

    map hissede constraintsler tenzimlendi

--- Globe Guide/Views/Globus/MapViewController.swift
+++ Globe Guide/Views/Globus/MapViewController.swift
@@ -1,39 +1,34 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UISearchBarDelegate {
 
     var customMap: Map!
     var searchBar: UISearchBar!
     let randomButton = UIButton(type: .system)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupCustomMap()
-        setupSearchBar()
-        setupRandomButton()
+        setupSearchBarElements()
+        setupRandomButtonElements()
+        setupConstraintsForButtonAndSearch()
     }
 
+   
     func setupCustomMap() {
         customMap = Map(frame: view.bounds)
-       // customMap.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(customMap)
-
-      
         let paris = CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522)
         customMap.addMarker(coordinate: paris, title: "Paris")
-
         customMap.setRegion(coordinate: paris, zoom: 5)
     }
-}
 
-
-extension MapViewController:UISearchBarDelegate{
-   func setupSearchBar() {
+    func setupSearchBarElements() {
         searchBar = UISearchBar()
         searchBar.placeholder = "Search for a country"
         searchBar.delegate = self
-       
         searchBar.backgroundImage = UIImage()
         searchBar.searchTextField.backgroundColor = .white
         searchBar.searchTextField.layer.cornerRadius = 18
@@ -42,42 +37,35 @@ extension MapViewController:UISearchBarDelegate{
         searchBar.searchTextField.leftView?.tintColor = .gray
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(searchBar)
-
-        NSLayoutConstraint.activate([
-            searchBar.leadingAnchor.constraint(equalTo: randomButton.trailingAnchor, constant: 10),
-               searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-               searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-               searchBar.heightAnchor.constraint(equalToConstant: 50)
-        ])
     }
-
-}
-
-extension MapViewController{
-    func setupRandomButton() {
-      
+ 
+    func setupRandomButtonElements() {
         randomButton.setTitle("Random", for: .normal)
+        randomButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         randomButton.backgroundColor = .systemBlue
         randomButton.setTitleColor(.white, for: .normal)
-        randomButton.layer.cornerRadius = 25
+        randomButton.layer.cornerRadius = 10
         randomButton.addTarget(self, action: #selector(randomButtonTapped), for: .touchUpInside)
-        
-        
         randomButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(randomButton)
-        
+    }
+
+    func setupConstraintsForButtonAndSearch() {
         NSLayoutConstraint.activate([
-            randomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            randomButton.trailingAnchor.constraint(equalTo:searchBar.leadingAnchor),
-            randomButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            randomButton.widthAnchor.constraint(equalToConstant: 100),
-            randomButton.heightAnchor.constraint(equalToConstant: 30)
+            randomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            randomButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            randomButton.heightAnchor.constraint(equalToConstant: 40),
+        
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            searchBar.heightAnchor.constraint(equalToConstant: 50),
+          
+            searchBar.leadingAnchor.constraint(equalTo: randomButton.trailingAnchor, constant: 5)
         ])
     }
 
     @objc func randomButtonTapped() {
         print("Random button tapped")
-      
     }
-
 }
+
